e163aeea1e79278ddbff53fd87bd6b19bdacaf2e
VianaMarcioDev <marcioviana.developer@gmail.com>
2025-01-29 22:22:49 -0300

    Aula sobre operadores

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03Operadores.java
@@ -0,0 +1,50 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03Operadores {
+    public static void main(String[] args) {
+        int num1 = 10;
+        String num2 = "20";
+        System.out.println(num1 + num2);
+
+        /*
+
+        Operadores aritméticos
+            + -> soma ou concatenação
+             -> se a declaração começar com uma String
+             , for uma String, o sinal de + e um número ou se for uma declaração com várias Strings
+             , o sinal de + fará uma concatenação e não uma soma
+
+            - -> subtração
+            / -> divisão
+            * -> multiplicação
+            % -> módulo ou resto da divisão
+
+         */
+
+        /*
+
+        Operadores relacionais
+
+        <  -> menor
+        >  -> maior
+        <= -> menor ou igual
+        >= -> maior ou igual
+        == -> igualdade
+        != -> diferença
+
+         */
+
+        /*
+
+        Operadores Lógicos
+
+        && -> and
+        || -> or
+        !  -> not
+
+         */
+
+
+
+    }
+}
