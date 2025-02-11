8d2228b4621408a69419ceeb1e3e56f8579600e6
VianaMarcioDev <marcioviana.developer@gmail.com>
2025-02-11 20:13:34 -0300

    Aprendendo sobre estrurura de repetição for imprimindo numeros pares

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07EstruturasDeRepeticaoFor.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07EstruturasDeRepeticaoFor {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 10 ; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
